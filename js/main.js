97997f27bbbc9815c327c44a69a0f83cada61a6b
Anne Ganzeboom <anneganzeboom@gmail.com>
2018-08-29 21:08:34 +0200

    working photo grid

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+//     $('.grid').masonry({
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 400
+// });
+
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: '.grid-sizer',
+//   percentPosition: true
+// });
+
+//////////////////////////////
+
+// init Masonry
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   percentPosition: true,
+//   columnWidth: '.grid-sizer'
+// });
+// layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry();
+// }); 
+
+//////////////////////////////
+
+imagesLoaded('.js-images-loaded', () => {
+  new Masonry(document.querySelector('.js-masonry'), {
+    itemSelector: '.js-masonry-item'
+  });
+});
+ 
 });
